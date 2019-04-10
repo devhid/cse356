a90721f700c15bd018e30591e5b5cdce3586bd37
devhid <devhid@users.noreply.github.com>
2019-04-10 03:34:11 -0400

    Pushed hw7

---
+++ hw7-mysql-memcached/database.js
@@ -0,0 +1,102 @@
+/**
+ * GP: Games Played, 
+ * GS: Games Started, 
+ * A: Assists, 
+ * GWA: Game-Winning Assists
+ * HmA:
+ * RdA: 
+ * A/90min: Assists / 90 Minutes
+ */
+
+const mysql = require('mysql');
+
+const connectionOptions = {
+    host: '127.0.0.1',
+    port: '3306',
+    database: 'hw7'
+};
+
+const connection = mysql.createConnection(connectionOptions);
+  
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+  
+    console.log('Connected as id ' + connection.threadId);
+});
+
+async function getAssistStatistics(club, position) {
+    const query = "select player, a, gs from assists where club = ? and pos = ?";
+
+    return new Promise((resolve, reject) => {
+        connection.query(query, [club, position], function(error, results, fields) {
+            if(error) {
+                console.log("[DEBUG] : Query encountered an error.");
+                reject(error);
+            } else {
+                if(results.length == 0) {
+                    console.log("[DEBUG} : Results array is empty.");
+                    resolve(null);
+                } else {
+                    /* calculate max assists, avg assists, and player who scored the most assists */
+                    let max_assists = 0;
+                    let avg_assists = 0;
+                    let player = result[0];
+
+                    console.log(`[DEBUG]: Initialized field, 'max_assists' = ${max_assists}.`);
+                    console.log(`[DEBUG]: Initialized field, 'avg_assists' = ${avg_assists}.`);
+                    console.log(`[DEBUG] Initialized field, 'player' = ${player}.`);
+
+                    console.log("[DEBUG] : Starting to calculate statistics...");
+                    for(let result of results) {
+                        if(result.a === max_assists) {
+                            console.log("[DEBUG] result.assists === max_assists.");
+                            console.log(`[DEBUG] Field, 'result.gs' = ${result.gs}`);
+                            console.log(`[DEBUG] Field, 'player.gs' = ${player.gs}`);
+
+                            player = (result.gs > player.gs) ? result : player;
+
+                            console.log(`[DEBUG] Field, 'player' = ${player}.`);
+                        }
+
+                        if(result.a > max_assists) {
+                            console.log("[DEBUG] result.assists > result.max_assists.");
+
+                            max_assists = result.a;
+                            player = result;
+
+                            console.log(`[DEBUG]: Field, 'max_assists' = ${max_assists}.`);
+                            console.log(`[DEBUG]: Field, 'player' = ${player}.`);
+                        }
+
+                        avg_assists += result.a;
+                        console.log(`[DEBUG]: Field, 'avg_assists' = ${avg_assists}.`);
+                    }
+
+                    avg_assists = avg_assists / results.length;
+                    player = player.player;
+
+                    console.log(`[DEBUG]: Field, 'avg_assists' = ${avg_assists}.`);
+                    console.log(`[DEBUG]: Field, 'player' = ${player}.`);
+
+                    let stats = {
+                        "club": club,
+                        "pos": position,
+                        "max_assists": max_assists,
+                        "player": player,
+                        "avg_assists": avg_assists
+                    }
+
+                    resolve(stats);
+                }
+            }
+        })
+    });
+}
+
+module.exports = {
+    getAssistStatistics: getAssistStatistics
+}
+ 
---
+++ hw7-mysql-memcached/index.js
@@ -0,0 +1,47 @@
+/* library imports */
+const express = require('express');
+
+/* internal imports */
+const database = require('./database');
+
+/* app instance */
+const app = express();
+require('express-async-errors');
+
+/* use json middleware */
+app.use(express.json());
+
+/* url to listen on */
+const URL = { host: 'http://127.0.0.1', port: 5000 };
+
+app.get('/hw7', async (req, res) => {
+    let response = {"status": "error", "message": ""};
+
+    const club = req.query['club'];
+    const position = req.query['pos'];
+
+    if(club === undefined || position == undefined) {
+        response["message"] = "Fields 'club' or 'pos' is undefined in query."
+        return res.json(response);
+    }
+
+    const assistStatistics = await database.getAssistStatistics(club, position);
+    if(assistStatistics === null) {
+        response["message"] = "Specified club or pos fields are invalid.";
+        return res.json(response);
+    }
+
+    res.json(assistStatistics);
+});
+
+function generateOK() {
+    return {
+        "club": "",
+        "pos": "",
+        "max_assists": 0,
+        "player": "",
+        "avg_assists": 0
+    };
+}
+
+app.listen(URL.port, URL.host, () => console.log(`Listening on ${URL.host}:${URL.port}...`));
