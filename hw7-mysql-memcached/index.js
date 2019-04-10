c3782d87d90b883875bc279bbe34a95a67a07f81
devhid <devhid@users.noreply.github.com>
2019-04-10 18:41:56 -0400

    memcached support

--- hw7-mysql-memcached/index.js
+++ hw7-mysql-memcached/index.js
@@ -1,5 +1,6 @@
 /* library imports */
 const express = require('express');
+const memcached = require('memcached')("127.0.0.1:11211");
 
 /* internal imports */
 const database = require('./database');
@@ -15,6 +16,13 @@ app.use(express.json());
 const URL = { host: '127.0.0.1', port: 5000 };
 
 app.get('/hw7', async (req, res) => {
+    const key = club + "|" + pos;
+
+    const cache = await getCache(key);
+    if(cache != null) {
+        return res.json(cache);
+    }
+    
     let response = {"status": "error", "message": ""};
 
     const club = req.query['club'];
@@ -30,6 +38,8 @@ app.get('/hw7', async (req, res) => {
         response["message"] = "Specified club or pos fields are invalid.";
         return res.json(response);
     }
+    
+    memcached.add(key, assistStatistics, 60, (err) => console.log(err)).
 
     res.json(assistStatistics);
 });
@@ -42,6 +52,18 @@ function generateOK() {
         "player": "",
         "avg_assists": 0
     };
+}
+
+async function getCache(key) {
+    return new Promise((resolve, reject) => {
+        memcached.get(key, (err, data) => {
+            if(err) {
+                resolve(null);
+            } else {
+                resolve(data);
+            }
+        });
+    });
 }
 
 app.listen(URL.port, URL.host, () => console.log(`Listening on http://${URL.host}:${URL.port}...`));
